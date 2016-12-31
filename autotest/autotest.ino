637c4ff8797ff24f6d8f115f4c3951fe879d943e
wendeurick <obelonave@gmail.com>
2016-12-31 08:53:26 -0200

    verificar display F e G

--- autotest/autotest.ino
+++ autotest/autotest.ino
@@ -1,1 +1,34 @@
+#include <SPI.h>
+#include "GaragemHackerDojoPins.h"
 
+void setup() {
+  pinMode(GH_SS_PIN, OUTPUT);
+  SPI.begin();
+}
+
+void loop() {
+  static byte i = 0;
+  displayWrite(i++);
+  if (i == 11) i = 0;
+  delay(250);
+}
+
+void displayWrite(byte i) {
+  byte digito[] = {
+    GH_DISPLAY_DIG_0,
+    GH_DISPLAY_DIG_1,
+    GH_DISPLAY_DIG_2,
+    GH_DISPLAY_DIG_3,
+    GH_DISPLAY_DIG_4,
+    GH_DISPLAY_DIG_5,
+    GH_DISPLAY_DIG_6,
+    GH_DISPLAY_DIG_7,
+    GH_DISPLAY_DIG_8,
+    GH_DISPLAY_DIG_9,
+    GH_DISPLAY_SEG_P
+  };
+
+  digitalWrite(GH_SS_PIN, LOW);
+  SPI.transfer(digito[i]);
+  digitalWrite(GH_SS_PIN, HIGH);
+}
